be5fb79dbd74aec12dcbe247a54e4a64e73eb157
Prashamtogadiya <pktogadiya222@gmail.com>
2025-07-16 08:58:08 +0530

    first commit

--- demo.java
+++ demo.java
@@ -1,14 +1,6 @@
 public class Demo{
     public static void main(String[] args) {
-        sout
-        as zmx cnmz xc
-        as
-        as
-        as
-        as'
-        asasas
-        as
-        as
+        \\
         as
         as
         as
