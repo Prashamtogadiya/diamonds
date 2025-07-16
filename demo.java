195644f01b87eed14c3ff9fe9195983a0b19dee0
Prashamtogadiya <pktogadiya222@gmail.com>
2025-07-16 09:06:13 +0530

    first commit

--- demo.java
+++ demo.java
@@ -3,18 +3,6 @@ public class Demo{
         \\
         as
         as
-        as
-        a
-        s
-        as
-        a
-        argsas
-        System.err.println(as
-        as
-        asas
-        a
-        System.err.println(as););
-        s
-        as
+      
     }
 }
